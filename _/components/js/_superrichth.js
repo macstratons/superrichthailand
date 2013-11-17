099250de16c327c0d2af6610387e5d954c65daa3
Mac Stratons <mac@stratonstech.com>
2013-11-18 01:45:20 +0700

    Create currency exchange rate.

--- _/components/js/_superrichth.js
+++ _/components/js/_superrichth.js
@@ -38,3 +38,36 @@ $(function() {
 	    }
 	});
 });
+
+// Charts Scripts
+Morris.Line({
+        element: 'morris-chart-line',
+        data: [
+          { y: '2006', a: 100, b: 90 },
+          { y: '2007', a: 75,  b: 65 },
+          { y: '2008', a: 50,  b: 40 },
+          { y: '2009', a: 75,  b: 65 },
+          { y: '2010', a: 50,  b: 40 },
+          { y: '2011', a: 40,  b: 50 },
+          { y: '2012', a: 30, b: 42 }
+        ],
+        xkey: 'y',
+        ykeys: ['a', 'b'],
+        labels: ['Series A', 'Series B']
+});
+
+Morris.Bar({
+        element: 'bar-example',
+        data: [
+          { y: '2006', a: 100, b: 90 },
+          { y: '2007', a: 75,  b: 65 },
+          { y: '2008', a: 50,  b: 40 },
+          { y: '2009', a: 75,  b: 65 },
+          { y: '2010', a: 50,  b: 40 },
+          { y: '2011', a: 75,  b: 65 },
+          { y: '2012', a: 100, b: 90 }
+        ],
+        xkey: 'y',
+        ykeys: ['a', 'b'],
+        labels: ['Series A', 'Series B']
+});
