8414f3ef7453c656d9c6fea7e93fc7e7f2f5d12a
Mac Stratons <mac@stratonstech.com>
2013-12-12 09:34:31 +0700

    Test DB-Surawong Fonts

---
+++ _/components/php/header.php
@@ -0,0 +1,8 @@
+<h1>SUPERRICH THAILAND</h1>
+<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non<p>
+<p>Super Rich Thailand บริษัท ซุปเปอร์ริช (ประเทศไทย) จำกัด</p>
+<p>Super Rich Thailand บริษัท ซุปเปอร์ริช (ประเทศไทย) จำกัด</p>
--- index.php
+++ index.php
@@ -2,15 +2,17 @@
 <html>
   <head>
     <title>Super Rich Thailand -- Home</title>
-    <link href='http://fonts.googleapis.com/css?family=Lato:900|Roboto:400,300,500,700' rel='stylesheet' type='text/css'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <!-- Bootstrap Core CSS -->
     <link href="_/css/bootstrap.css" rel="stylesheet">
     <link href="_/css/superrichth.css" rel="stylesheet">
   </head>
   
-  <body>
+  <body id="home">
 
+    <!-- NAVBAR -->
+    <?php include '_/components/php/header.php'; ?>
 
     <script src="_/js/bootstrap.js"></script>
     <script src="_/js/superrichth.js"></script>
