ebebb564ff1e5b0f27728a0f57decb36b5dff484
Mac Stratons <mac@stratonstech.com>
2013-12-25 08:01:24 +0700

    New navigation bar.

---
+++ _/components/php/header.php
@@ -0,0 +1,47 @@
+<header id="header">
+  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-sr-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" id="logo" href="#"><img src="images/sr-logo.png" class="img-responsive"></a>
+      </div>
+      <!-- /.navbar-header -->
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse navbar-sr-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <div class="contacts_block">
+            <!-- Edit social icons here -->
+            <ul class="social-icons hidden-xs">
+              <li><a href="https://www.facebook.com/SuperRichThailand"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="https://twitter.com/superrichTH"><i class="fa fa-twitter"></i></a></li>
+            </ul>
+            <!-- Edit phone number here -->
+            <div class="social-icons"><a href="#"><i class="fa fa-facebook"></i></a></div>
+            <div class="social-icons"><a href="#"><i class="fa fa-twitter"></i></a></div>
+            <div class="phone"><i class="fa fa-phone"></i> +66 (0)2 254 4444</div>
+            <div class="clear"></div>
+            <!-- ./phone -->
+          </div>
+          <li><a href="index.php">HOME</a></li>
+          <li><a href="exchange.php">EXCHANGE RATE</a></li>
+          <li><a href="about.php">ABOUT US</a></li>
+          <li><a href="contact.php">CONTACT</a></li>
+          <li><a href="news.php">NEWS UPDATE</a></li>
+          <li><a href="faq.php">FAQ</a></li>
+        </ul>
+        <!-- /.nav -->
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+  </nav>
+  <!-- /.navbar -->
+</header>
+<!-- /#header -->
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Super Rich Thailand -- Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap Core CSS -->
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/superrichth.css" rel="stylesheet">
+  </head>
+  
+  <body>
+    <!-- HEADER START -->
+    <?php include '_/components/php/header.php'; ?>
+
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/superrichth.js"></script>
+
+  </body>
+</html>
