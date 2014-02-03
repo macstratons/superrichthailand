aa6fa2fcf6ba6118c4ed380de769cb0d6386d238
Mac Stratons <mac@stratonstech.com>
2014-02-03 10:56:03 +0700

    IE8 support of HTML5
    HTML5 Shim and Respond.js IE8 support of HTML5 elements and media
    queries

--- about.php
+++ about.php
@@ -2,8 +2,10 @@
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>Super Rich Thailand -- About Us</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap Core CSS -->
     <link href="_/css/bootstrap.css" rel="stylesheet">
     <link href="_/css/superrichth.css" rel="stylesheet">
@@ -13,7 +15,7 @@
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
   
@@ -33,6 +35,7 @@
     <!-- FOOTER -->
     <?php include '_/components/php/footer.php'; ?>
 
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="_/js/bootstrap.js"></script>
     <script src="_/js/superrichth.js"></script>
 
--- exchange.php
+++ exchange.php
@@ -2,8 +2,10 @@
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>Super Rich Thailand -- Exchange Rate</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap Core CSS -->
     <link href="_/css/bootstrap.css" rel="stylesheet">
     <link href="_/css/superrichth.css" rel="stylesheet">
@@ -18,7 +20,7 @@
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
   
@@ -83,6 +85,7 @@
     <!-- Scroll To Top --> 
     <a href="#" class="scrollup"><i class="fa fa-angle-up"></i></a>
 
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="_/js/bootstrap.js"></script>
     <script src="_/js/superrichth.js"></script>
 
